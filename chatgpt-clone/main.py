43cd1d9b780285a3745743f1919a9b9eb2d7fbcf
funky1119 <funky1119@naver.com>
2026-02-17 07:24:17 +0900

    #8.7 Hosted MCP Tool 완료

--- chatgpt-clone/main.py
+++ chatgpt-clone/main.py
@@ -45,7 +45,19 @@ if "agent" not in st.session_state:
                 }
             ),
             CodeInterpreterTool(
-                tool_config={"type": "code_interpreter", "container": "auto"}
+                tool_config={
+                    "type": "code_interpreter",
+                    "container": {"type": "auto"},
+                }
+            ),
+            HostedMCPTool(
+                tool_config={
+                    "server_url": "https://mcp.context7.com/mcp",
+                    "type": "mcp",
+                    "server_label": "Context7",
+                    "server_description": "Use this tool get the doc from software projects.",
+                    "require_approval": "never",
+                }
             ),
         ],
     )
@@ -106,16 +118,29 @@ async def paint_history():
             if message_type == "web_search_call":
                 with st.chat_message("ai"):
                     st.write("🔎 Searched the web...")
+
             elif message_type == "file_search_call":
                 with st.chat_message("ai"):
                     st.write("🗂️ Searched files...")
+
             elif message_type == "image_generation_call":
                 image = base64.b64decode(message["result"])
                 with st.chat_message("ai"):
                     st.image(image)
+
             elif message_type == "code_interpreter_call":
                 with st.chat_message("ai"):
                     st.code(message["code"])
+
+            elif message_type == "mcp_list_tools":
+                with st.chat_message("ai"):
+                    st.write(f"List {message['server_label']}'s tools")
+
+            elif message_type == "mcp_call":
+                with st.chat_message("ai"):
+                    st.write(
+                        f"Called {message['server_label']}'s {message['name']} with args {message['arguments']}"
+                    )
 
 
 asyncio.run(paint_history())
@@ -159,6 +184,30 @@ def update_status(status_container, event):
         "response.code_interpreter_call.interpreting": (
             "🤖 Running code...",
             "complete",
+        ),
+        "response.mcp_call.completed": (
+            "⚒️ Called MCP tool",
+            "complete",
+        ),
+        "response.mcp_call.failed": (
+            "⚒️ Error calling MCP tool",
+            "complete",
+        ),
+        "response.mcp_call.in_progress": (
+            "⚒️ Calling MCP tool...",
+            "running",
+        ),
+        "response.mcp_list_tools.completed": (
+            "⚒️ Listed MCP tools",
+            "complete",
+        ),
+        "response.mcp_list_tools.failed": (
+            "⚒️ Error listing MCP tools",
+            "complete",
+        ),
+        "response.mcp_list_tools.in_progress": (
+            "⚒️ Listing MCP tools",
+            "running",
         ),
         "response.completed": ("✅", "complete"),
     }
