d5e3b84e2af5b3c0c2c7858852ae96cbec5cfb27
funky1119 <funky1119@naver.com>
2026-03-28 22:10:32 +0900

    #17.5 Testing AI Nodes

--- testing-agents/tests.py
+++ testing-agents/tests.py
@@ -1,47 +1,42 @@
+import dotenv
+dotenv.load_dotenv()
+
 import pytest
 from main import graph
 
 @pytest.mark.parametrize(
-    "email, expected_category, expected_score",
+    "email, expected_category, min_score, max_score",
     [
-        ("this is urgent!", "urgent", 10),
-        ("i wanna talk to you", "normal", 5),
-        ("i have an offer for you", "spam", 1),
+        ("this is urgent!", "urgent", 8, 10),
+        ("i wanna talk to you", "normal", 4, 7),
+        ("i have an offer for you", "spam", 1, 3),
     ]
 )
 
 # 전체 노드 테스트
-def test_full_graph(email, expected_category, expected_score):
+def test_full_graph(email, expected_category, min_score, max_score):
     result = graph.invoke(
         {"email": email},
         config={"configurable": {"thread_id": "1"}}
     )
 
     assert result["category"] == expected_category
-    assert result["priority_score"] == expected_score
+    assert min_score <= result["priority_score"] <= max_score
 
 # 노드 개별 테스트
 def test_individual_nodes():
-    # categorize_email
-    result = graph.nodes["categorize_email"].invoke({
-        "email": "check out his offer", 
-    })
-
-    assert result["category"] == "spam"
-    
+   
     # assing_priority
     result = graph.nodes["assing_priority"].invoke({
         "category": "spam", 
+        "email": "buy this pot."
     })
 
-    assert result["priority_score"] == 1
+    assert 1 <= result["priority_score"] <= 3
     
     # draft_response
-    result = graph.nodes["draft_response"].invoke({
-        "category": "spam", 
-    })
-
-    assert "저리 가" in result["response"]
+    #result = graph.nodes["draft_response"].invoke({ "category": "spam" })
+    #assert "저리 가" in result["response"]
 
 
 def test_partial_execution():
@@ -68,4 +63,4 @@ def test_partial_execution():
         interrupt_after="draft_response",
     )
 
-    assert result["priority_score"] == 1
+    assert 1 <= result["priority_score"] <= 3
