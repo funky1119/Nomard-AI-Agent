ea02461e0d031bf37b8a538e8aa739bd4e591061
funky1119 <funky1119@naver.com>
2026-04-12 15:14:33 +0900

    #21.1 Conversations API

--- deploying-agents/main.py
+++ deploying-agents/main.py
@@ -1,6 +1,31 @@
-def main():
-    print("Hello from deploying-agents!")
+from dotenv import load_dotenv
+load_dotenv()
+
+from agents import Agent, Runner
+from fastapi import FastAPI
+from openai import AsyncOpenAI
+from pydantic import BaseModel
+
+agent = Agent(
+  name="Assistant", 
+  instructions="너는 사용자의 질문에 답변한다.",
+)
+
+app = FastAPI()
+
+client = AsyncOpenAI()
+
+class CreateConversationResponse(BaseModel):
+  conversation_id: str
+
+@app.post("/conversations")
+async def create_conversation() -> CreateConversationResponse:
+  conversation = await client.conversations.create()
+  return {
+    'conversation_id': conversation.id,
+  }
 
 
-if __name__ == "__main__":
-    main()
+@app.post("/conversations/{conversation_id}/message")
+async def create_message(conversation_id: str):
+  pass
